d93602bef801561882b2cd76c31de2286b46c492
wjn <wjntest@163.com>
2019-07-14 18:50:51 +0800

    update readconfig.py

--- common/readconfig.py
+++ common/readconfig.py
@@ -3,6 +3,7 @@ import os
 def getbrowsername(name):
     cf = configparser.ConfigParser()
     cfpath=os.path.dirname(os.path.abspath('.'))+'/config/config.ini'
+    #print(cfpath)
     cf.read(cfpath)
     browsername = cf.get('browser',name)
     return browsername
